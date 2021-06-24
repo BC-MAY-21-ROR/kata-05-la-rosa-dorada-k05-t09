2d2e191c9c0311da324fb7036755558e6340e990
Severo_iglesias <siglesias@ucol.mx>
2021-06-24 18:19:07 -0500

    Pruebas de rubycritic, sandi-meter y se aplico rubocop,
    Co-authored-by: SEVERO96 <siglesias@ucol.mx>
    Co-authored-by: SaraNavarro  <esaranm@gmail.com>

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,22 +1,25 @@
+# frozen_string_literal: true
 
+# Comentario de prueba
 class GildedRose
   def initialize(items)
     @items = items
   end
 
   def update_quality
-      @items.each(&method(:update))
-      self
+    @items.each(&method(:update))
+    self
   end
 
   def update(item)
-    if item.name == "Sulfuras, Hand of Ragnaros"
+    case item.name
+    when 'Sulfuras, Hand of Ragnaros'
       SulfurasUpdater.new(item).update
-    elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+    when 'Backstage passes to a TAFKAL80ETC concert'
       BackstagePassesUpdater.new(item).update
-    elsif item.name == "Aged Brie"
+    when 'Aged Brie'
       AgedBrieUpdater.new(item).update
-    elsif item.name == "Conjured Mana Cake"
+    when 'Conjured Mana Cake'
       ConjuredUpdater.new(item).update
     else
       BasicItemUpdater.new(item).update
@@ -24,6 +27,7 @@ class GildedRose
     nil
   end
 
+  # Comentario de prueba
   class SulfurasUpdater
     attr_reader :item
 
@@ -31,10 +35,10 @@ class GildedRose
       @item = item
     end
 
-    def update
-    end
+    def update; end
   end
 
+  # Comentario de prueba
   class BasicItemUpdater
     attr_reader :item
 
@@ -43,31 +47,27 @@ class GildedRose
     end
 
     def update
-      if item.quality > 0
-        item.quality -= 1
-      end
+      item.quality -= 1 if item.quality.positive?
       item.sell_in -= 1
-      if item.sell_in < 0
-        if item.quality > 0
-          item.quality -= 1
-        end
-      end
+      item.quality -= 1 if item.sell_in.negative? && item.quality.positive?
     end
   end
 
+  # Comentario de prueba
   class ConjuredUpdater
     attr_reader :item
-    def initialize (item)
+
+    def initialize(item)
       @item = item
     end
+
     def update
-    if item.quality > 0
-      item.quality /= 2
-    end
-    item.sell_in -= 1
+      item.quality /= 2 if item.quality.positive?
+      item.sell_in -= 1
     end
   end
 
+  # Comentario de prueba
   class AgedBrieUpdater
     attr_reader :item
 
@@ -76,18 +76,13 @@ class GildedRose
     end
 
     def update
-      if item.quality < 50
-        item.quality += 1
-      end
+      item.quality += 1 if item.quality < 50
       item.sell_in -= 1
-      if item.sell_in < 0
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
+      item.quality += 1 if item.sell_in.negative? && (item.quality < 50)
     end
   end
 
+  # Comentario de prueba
   class BackstagePassesUpdater
     attr_reader :item
 
@@ -98,26 +93,16 @@ class GildedRose
     def update
       if item.quality < 50
         item.quality += 1
-        if item.sell_in < 11
-          if item.quality < 50
-            item.quality += 1
-          end
-        end
-        if item.sell_in < 6
-          if item.quality < 50
-            item.quality += 1
-          end
-        end
+        item.quality += 1 if item.sell_in < 11 && (item.quality < 50)
+        item.quality += 1 if item.sell_in < 6 && (item.quality < 50)
       end
       item.sell_in -= 1
-      if item.sell_in < 0
-        item.quality -= item.quality
-      end
+      item.quality -= item.quality if item.sell_in.negative?
     end
   end
-
 end
 
+# Comentario de prueba
 class Item
   attr_accessor :name, :sell_in, :quality
 
@@ -130,61 +115,4 @@ class Item
   def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
-
 end
-
-
-
-
-
-    
-  
-=begin
-
-Bienvenido al equipo de Gilded Rose. Como quizá sabes, somos una pequeña posada 
-ubicada estratégicamente en una prestigiosa ciudad, atendida por la amable Allison. 
-También compramos y vendemos mercadería de alta calidad. Por desgracia, nuestra mercadería 
-va bajando de calidad a medida que se aproxima la fecha de venta.
-
-Tenemos un sistema instalado que actualiza automáticamente el inventario. 
-Este sistema fue desarrollado por un muchacho con poco sentido común llamado Leeroy, 
-que ahora se dedica a nuevas aventuras. Tu tarea es agregar una nueva característica 
-al sistema para que podamos comenzar a vender una nueva categoría de items.
-
-Descripción preliminar
-Pero primero, vamos a introducir el sistema:
-
-Todos los artículos (Item) tienen una propiedad sellIn que denota el número de días que tenemos para venderlo
-Todos los artículos tienen una propiedad quality que denota cúan valioso es el artículo
-Al final de cada día, nuestro sistema decrementa ambos valores para cada artículo mediante el método updateQuality
-Bastante simple, ¿no? Bueno, ahora es donde se pone interesante:
-
-Una vez que ha pasado la fecha recomendada de venta, la calidad se degrada al doble de velocidad
-La calidad de un artículo nunca es negativa
-El "Queso Brie envejecido" (Aged brie) incrementa su calidad a medida que se pone viejo
-Su calidad aumenta en 1 unidad cada día
-luego de la fecha de venta su calidad aumenta 2 unidades por día
-La calidad de un artículo nunca es mayor a 50
-El artículo "Sulfuras" (Sulfuras), siendo un artículo legendario, no modifica su fecha de venta ni se degrada en calidad
-Una "Entrada al Backstage", como el queso brie, incrementa su calidad a medida que la fecha de venta se aproxima
-si faltan 10 días o menos para el concierto, la calidad se incrementa en 2 unidades
-si faltan 5 días o menos, la calidad se incrementa en 3 unidades
-luego de la fecha de venta la calidad cae a 0
-El requerimiento
-Hace poco contratamos a un proveedor de artículos conjurados mágicamente. Esto requiere una actualización 
-del sistema:
-
-Los artículos conjurados degradan su calidad al doble de velocidad que los normales
-Siéntete libre de realizar cualquier cambio al mensaje updateQuality y agregar el 
-código que sea necesario, mientras que todo siga funcionando correctamente. Sin embargo, 
-no alteres el objeto Item ni sus propiedades ya que pertenecen al goblin que está en ese rincón, 
-que en un ataque de ira te va a liquidar de un golpe porque no cree en la cultura de código compartido.
-
-Notas finales
-Para aclarar: un artículo nunca puede tener una calidad superior a 50, sin embargo las Sulfuras siendo un 
-artículo legendario posee una calidad inmutable de 80.
-
--------------------------------------------------------------------------
-    
-=end
-
--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,4 +1,3 @@
-
 require 'rspec'
 require_relative '../ruby/gilded_rose'
 
@@ -12,24 +11,18 @@ RSpec.describe GildedRose do
   end
 
   describe ' Ejemplo ' do
-  it ' Valor sell_in puede ser negativo ' do
-    items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 0)]
-    GildedRose.new(items).update_quality
-    expect(items[0].sell_in).to  eq (-1)
+    it ' Valor sell_in puede ser negativo ' do
+      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq(-1)
     end
   end
-  
-    describe ' Ejemplo ' do
+
+  describe ' Ejemplo ' do
     it ' Conjured Mana Cake baja su calidad el doble de rápido ' do
-    items = [Item.new('Conjured Mana Cake', 3, 6)]
-    GildedRose.new(items).update_quality
-    expect(items[0].quality).to eq (3) 
+      items = [Item.new('Conjured Mana Cake', 3, 6)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(3)
     end
   end
-
 end
-
-
-
-
-
